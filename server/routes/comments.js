466346e9b1b6f564b0981b13d6ad135c448737ea
as6769-2004 <adityasagar9991@gmail.com>
2025-09-24 00:57:57 +0530

    Update comments.js route

---
+++ server/routes/comments.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/commentsController");
+
+router.post("/:postId/comments", commentsController.addComment);
+router.delete("/:postId/comments", commentsController.deleteComment);
+
+module.exports = router;
