4931ffe1bbc7992ce2711515c742edbf09e1f0d8
as6769-2004 <adityasagar9991@gmail.com>
2025-09-24 00:58:09 +0530

    Update posts.js route

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+// Change this line:
+const postsController = require("../controllers/postsController");
+
+// Make sure ../controllers/postsController.js exists
+
+// Posts
+router.get("/", postsController.getPosts);
+router.post("/", postsController.createPost);
+router.delete("/:id", postsController.deletePost);
+
+module.exports = router;
